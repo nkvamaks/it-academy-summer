c011744f2fae18af34e4bb44a1835e17032b55b9
Maksim Kvach <55558386+nkvamaks@users.noreply.github.com>
2020-05-09 23:40:28 +0300

    A bit more lines

--- HW1_Maksim_Kvach.py
+++ HW1_Maksim_Kvach.py
@@ -4,3 +4,7 @@ print("Hi there, greetings from Maksim!")
 
 name = input("What's your name?\n")
 print("Hi, %s! Glad to hear from you!" % name)
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name1 in enumerate(friends):
+    print('My friend {iteration} is {name}'.format(iteration=i, name=name1))
