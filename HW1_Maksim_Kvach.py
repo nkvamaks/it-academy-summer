838df00808fd3f920d1c1aa4f24e0217e1b7c4b2
Maksim Kvach <55558386+nkvamaks@users.noreply.github.com>
2020-05-09 23:11:11 +0300

    Several lines of code, just for testing purposes

---
+++ HW1_Maksim_Kvach.py
@@ -0,0 +1,6 @@
+"""First Homework. Some simple programmes to train Python style."""
+
+print("Hi there, greetings from Maksim!")
+
+name = input("What's your name?\n")
+print("Hi, %s! Glad to hear from you!" % name)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # it-academy-python-summer
 Web development course for it-academy
+
+Class 1: Learning basic concepts of Python language and 'git' system.
