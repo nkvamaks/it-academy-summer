6ccb62308db5d56611c83e9c8710b004e87bb5a3
Maksim Kvach <55558386+nkvamaks@users.noreply.github.com>
2020-05-29 00:42:41 +0300

    Add Exercise 2 into a task7.py

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -1,6 +1,7 @@
-"""Tongues.
+"""Exercise 1: Tongues.
 
 https://www.codewars.com/kata/52763db7cffbc6fe8c0007f8
+
 Gandalf's writings have long been available for study, but no one has yet
 figured out what language they are written in. Recently, due to programming
 work by a hacker known only by the code name ROT13, it has been discovered
@@ -51,9 +52,46 @@ def tongues(str_):
     return str_.translate(transdict)  # Return translated string.
 
 
+"""Exercise 2: String -> N iterations -> String.
+
+We have a string s
+We have a number n
+Here is a function that takes your string, concatenates the even-indexed chars
+to the front, odd-indexed chars to the back.
+
+The Task:
+return the result of the string after applying the function to it n times.
+
+Example where s = "qwertyuio" and n = 2:
+after 1 iteration  s = "qetuowryi"
+after 2 iterations s = "qtorieuwy"
+return "qtorieuwy"
+"""
+
+
+def jumbled_string(s, n):
+    """Transform string.
+
+    :param s: string to transform.
+    :param n: int, how many times to repeat the transformation.
+    :return: transformed string.
+    """
+    for count_ in range(n):
+        s = ''.join([char for index, char in enumerate(s) if index % 2 == 0] +
+                    [char for index, char in enumerate(s) if index % 2 != 0])
+        return s
+
+
 if __name__ == '__main__':
     # Enter Gandalf's text from console
     print("Exercise 1: Gandalf's writings\n")
     # str_ = 'Ita dotf ni dyca nsaw ecc.'  # simple sample
     str_ = input('I was talking aloud to myself: ')
     print(tongues(str_))
+
+    # Enter text to transform from console
+    print("Exercise 2: One-two-three - trAnsphOOOrm!\n")
+    # str_ = 'Ita dotf ni dyca nsaw ecc.'  # simple sample
+    s = input('Type any string you want to transform: ')
+    n = int(input('How many times would you like to do that? '))
+    print(jumbled_string(s, n))
