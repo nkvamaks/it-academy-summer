43efac07bff6449721c3f71cad828dce26d63fd1
Maksim Kvach <55558386+nkvamaks@users.noreply.github.com>
2020-06-30 00:25:00 +0300

    Initial commit
    Seven tasks were created alongside with two text files:
    test_cities.txt and test_languages.txt that contain input
    data for corresponding tasks.

---
+++ src/homework4/1_dict_comprehensions.py
@@ -0,0 +1,15 @@
+"""
+Dict comprehensions.
+
+Создайте словарь с помощью генератора словарей, так чтобы его ключами были
+числа от 1 до 20, а значениями кубы этих чисел.
+"""
+
+
+def dict_compr():
+    """Return dict with integers 1-20 as keys and their cubes as values."""
+    return {num: num**3 for num in range(1, 21)}
+
+
+if __name__ == '__main__':
+    print(dict_compr())
