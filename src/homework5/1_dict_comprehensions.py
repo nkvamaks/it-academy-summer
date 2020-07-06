41b4e9ce677ef5ed2f4fae2ce738e5d7e28bb765
Maksim Kvach <55558386+nkvamaks@users.noreply.github.com>
2020-07-06 22:32:51 +0300

    Create runner.py

--- src/homework5/1_dict_comprehensions.py
+++ src/homework5/1_dict_comprehensions.py
@@ -9,7 +9,3 @@ Dict comprehensions.
 def dict_compr():
     """Return dict with integers 1-20 as keys and their cubes as values."""
     return {num: num**3 for num in range(1, 21)}
-
-
-if __name__ == '__main__':
-    print(dict_compr())
