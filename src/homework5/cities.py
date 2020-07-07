804732261551093146f49951fa3fb36ca98426b9
Maksim Kvach <55558386+nkvamaks@users.noreply.github.com>
2020-07-07 22:25:48 +0300

    Transform 'cities' task into a function

---
+++ src/homework5/cities.py
@@ -0,0 +1,31 @@
+"""Cities."""
+
+
+def cities(filename):
+    """
+    Города.
+
+    Входные данные поступают из файла 'filename' из текущей директории. В
+    файле дан список стран и городов каждой страны. В первой строке этого файла
+    содержится количество стран N. Далее идет N строк, каждая строка начинается
+    с названия страны, затем идут названия городов этой страны. В следующей
+    строке записано число M, далее идут M запросов — названия каких-то M
+    городов, перечисленных выше. Программа возвращает страну(ы), где находится
+    запрошенный город в виде строки.
+    """
+    relation = dict()  # stores relations: city-country(es)
+    output = ''
+
+    with open(filename) as f:
+        # from input file creates a sort of database with a relation
+        # city-country(es)
+        for _N in range(int(f.readline())):
+            country, *cities = f.readline().split()
+            for city in cities:
+                relation[city] = relation.get(city, []) + [country]
+
+            # return country(s) for the input city(es)
+        for _M in range(int(f.readline())):
+            city = f.readline().strip()
+            output += ' '.join(relation.get(city)) + '\n'
+    return output
--- src/homework5/runner.py
+++ src/homework5/runner.py
@@ -1,3 +1,14 @@
 """
+Функция runner.
 
+Вызывает функции, написанные в homework4:
+a. runner() – все фукнции вызываются по очереди.
+b. runner(‘func_name’) – вызывается только функцию func_name.
+c. runner(‘func’, ‘func1’...) - вызывает все переданные функции.
 """
+from dict_comprehensions import dict_compr
+from cities import cities
+
+print(dict_compr())
+
+print(cities('test_cities.txt'))
